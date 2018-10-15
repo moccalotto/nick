310a05b1f42889524e5329f89a63c5b2a0aae9a4
Kim Ravn Hansen <krh@cego.dk>
2018-10-15 13:18:41 +0200

    Allow strings with spaces and #s

--- machine/compile.go
+++ machine/compile.go
@@ -2,6 +2,7 @@ package machine
 
 import (
 	"errors"
+	"fmt"
 	"github.com/golang-collections/collections/stack"
 	"log"
 	"math/rand"
@@ -30,25 +31,74 @@ func MachineFromScript(p string) *Machine {
 	}
 }
 
-func lineToInstruction(l string) (*Instruction, error) {
-	l = strings.TrimSpace(l)
-	parts := strings.SplitN(l, "#", 2)
-	if len(parts) == 0 {
-		// string was empty or contained only whitespace
-		return nil, errors.New("Empty input string")
+func escapeString(s string) (string, []string) {
+	r := regexp.MustCompile(`\[\[[^\]]+\]\]`)
+	placeholders := []string{} // placeholders
+
+	// generate a temporary string where whitespace inside [[ ]] blocks is removed
+	escaped := r.ReplaceAllStringFunc(s, func(m string) string {
+		i := len(placeholders)
+		txt := m[2 : len(m)-2]
+
+		placeholders = append(placeholders, txt)
+
+		return fmt.Sprintf("___REP___%09d___PER___", i)
+	})
+
+	return escaped, placeholders
+}
+
+func unescapeString(s string, placeholders []string) string {
+	r := regexp.MustCompile(`___REP___\d{9}___PER___`)
+
+	return r.ReplaceAllStringFunc(s, func(m string) string {
+		var i int
+		_, err := fmt.Sscanf(m, "___REP___%09d___PER___", &i)
+
+		if err != nil {
+			return m
+		}
+
+		return placeholders[i]
+	})
+}
+
+func extractWords(s string, placeholders []string) []string {
+	// split the temporary string into words
+	splitted := strings.Fields(s)
+
+	res := make([]string, len(splitted))
+
+	// Replace the placeholders in each word with their original meaning
+	for i, word := range splitted {
+		res[i] = unescapeString(word, placeholders)
 	}
 
-	words := strings.Fields(parts[0])
+	return res
+}
 
+func extractCodeAndComment(s string) (string, string) {
+	parts := strings.SplitN(s, "#", 2)
+
+	code := strings.TrimSpace(strings.Replace(parts[0], "\\#", "#", -1))
 	comment := ""
 	if len(parts) == 2 {
 		comment = strings.TrimSpace(parts[1])
 	}
 
-	if len(words) < 1 {
-		// empty instruction (i.e. a line with only a comment) yields no Instruction
-		return nil, errors.New("Empty instruction")
+	return code, comment
+}
+
+func lineToInstruction(l string) (*Instruction, error) {
+	escaped, placeholders := escapeString(l)
+	code, comment := extractCodeAndComment(escaped)
+	if len(code) == 0 {
+		// string was empty or contained only whitespace
+		return nil, errors.New("Empty Code Block")
 	}
+
+	words := extractWords(code, placeholders)
+	comment = unescapeString(comment, placeholders)
 
 	return &Instruction{
 		Cmd:     words[0],
