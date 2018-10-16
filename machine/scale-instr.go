f9b713f01c8b7d474560fe4df7e90a2460ad85b7
Kim Ravn Hansen <krh@cego.dk>
2018-10-16 17:36:16 +0200

    Improved feedback in case of misuse.

--- machine/scale-instr.go
+++ machine/scale-instr.go
@@ -12,7 +12,7 @@ func Scale(m *Machine) {
 	m.Assert(m.Field != nil, "Cannot evolve a non-initialized field!")
 	x := m.ArgAsFloat(0)
 	y := x
-	if m.HasArg(2) {
+	if m.ArgCount() > 1 {
 		y = m.ArgAsFloat(2)
 		m.Assert(m.ArgAsString(1) == "x", "Second arg to scale must be an 'x', but '%s' was given")
 
