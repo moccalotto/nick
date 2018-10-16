f7679e519e80e88931603f245b1d483ce912ece8
Kim Ravn Hansen <krh@cego.dk>
2018-10-16 17:37:11 +0200

    In case stop! is executed, ensure that there is a field

--- machine/stop-instr.go
+++ machine/stop-instr.go
@@ -1,9 +1,14 @@
 package machine
 
+import "github.com/moccalotto/nick/field"
+
 func init() {
 	InstructionHandlers["stop!"] = Stop
 }
 
 func Stop(m *Machine) {
+	if m.Field == nil {
+		m.Field = field.NewField(1, 1)
+	}
 	m.State.PC = len(m.Tape)
 }
