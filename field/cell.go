0d929c54f6b5089c81f357e31272bfebfba100df
Kim Ravn Hansen <krh@cego.dk>
2018-10-21 15:12:09 +0200

    FIX: dead cells are not 2 - they are 0
    Also potential speedup

--- field/cell.go
+++ field/cell.go
@@ -6,15 +6,15 @@ type CellMapper func(f *Field, x, y int, c Cell) Cell
 
 func (c Cell) Alive() bool {
 	// Cells with value 1 or greater are considered to be alive
-	return c > 0
+	return c != DeadCell
 }
 
 func (c Cell) Dead() bool {
 	// Cells with value 0 or lower are considered to be dead.
 	// This means that there are several "dead" states.
 	// It also means that you cannot use fields to denote negative heights.
-	return c <= 0
+	return c == DeadCell
 }
 
+const DeadCell Cell = 0
 const LivingCell Cell = 1
-const DeadCell Cell = 2
