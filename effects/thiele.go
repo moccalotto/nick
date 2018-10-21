648ab541aa4481f885e8509e0ddfd36face1a07f
Kim Ravn Hansen <krh@cego.dk>
2018-10-21 16:15:17 +0200

    Potential minor speedup

--- effects/thiele.go
+++ effects/thiele.go
@@ -5,17 +5,11 @@ import (
 	"math"
 )
 
-type gauss struct {
-	x int
-	y int
-}
-
 type ThielePattern struct {
-	Base  complex128
-	Fill  bool // should we fill all of the map?
-	Alive bool
-	a     int // real composant
-	b     int // imaginary composant
+	Base complex128
+	Cell field.Cell
+	a    int // real composant
+	b    int // imaginary composant
 }
 
 func absInt(n int) int {
@@ -33,19 +27,11 @@ func cFloor(n complex128) complex128 {
 	)
 }
 
-func cGauss(n complex128) gauss {
-	return gauss{
+func cGauss(n complex128) field.Point {
+	return field.Point{
 		int(math.Round(real(n))),
 		int(math.Round(imag(n))),
 	}
-}
-
-func cRemaind(a, b complex128) gauss {
-	div := a / b
-	intpart := cFloor(div)
-	floatpart := div - intpart
-
-	return cGauss(floatpart * b)
 }
 
 func normalizeIntoQuadrant1(a, b int) (int, int) {
@@ -70,56 +56,39 @@ func NewThielePattern(a, b int) *ThielePattern {
 
 	return &ThielePattern{
 		complex(float64(a), float64(b)),
-		true,
-		true,
+		field.LivingCell,
 		a,
 		b,
 	}
 }
 
-// Get the points we need to investigate
-// we iterate over the area covered by the base (as a vector)
-// and the transposed base (as a vector).
-func (t *ThielePattern) pointsToInspect() <-chan complex128 {
-	ch := make(chan complex128)
-	go func() {
-		max := absInt(t.a) + absInt(t.b)
-		// naive solution: iterate over a suitably large area,
-		// don't mind where the area is located.
-		for y := 0; y <= max; y++ {
-			for x := 0; x <= max; x++ {
-				ch <- complex(float64(x), float64(y))
+func (t *ThielePattern) residueNumbers() field.Area {
+	result := field.Area{}
+	max := absInt(t.a) + absInt(t.b)
+
+	for y := 0; y <= max; y++ {
+		for x := 0; x <= max; x++ {
+			c := complex(float64(x), float64(y))
+			// square the point, and modulo it into the base area
+			p := t.remaind(c * c)
+			alreadyFound := false
+
+			for _, n := range result {
+				if n == p {
+					alreadyFound = true
+					break
+				}
 			}
-		}
-		close(ch)
-	}()
 
-	return ch
-}
-
-func (t *ThielePattern) residueNumbers() []gauss {
-	numbers := []gauss{}
-
-	for c := range t.pointsToInspect() {
-		// square the point, and modulo it into the base area
-		p := cRemaind(c*c, t.Base)
-		alreadyFound := false
-
-		for _, n := range numbers {
-			if n == p {
-				alreadyFound = true
-				break
+			if alreadyFound {
+				continue
 			}
-		}
 
-		if alreadyFound {
-			continue
+			result = append(result, p)
 		}
-
-		numbers = append(numbers, p)
 	}
 
-	return numbers
+	return result
 }
 
 func (t *ThielePattern) mapper(f *field.Field, x, y int, c field.Cell) field.Cell {
@@ -127,7 +96,7 @@ func (t *ThielePattern) mapper(f *field.Field, x, y int, c field.Cell) field.Cel
 	numbers := t.residueNumbers()
 
 	c0 := complex(float64(x), float64(y))
-	c1 := cRemaind(c0, t.Base)
+	c1 := t.remaind(c0)
 
 	for _, c := range numbers {
 		if c == c1 {
@@ -139,34 +108,27 @@ func (t *ThielePattern) mapper(f *field.Field, x, y int, c field.Cell) field.Cel
 	return c
 }
 
-func (t *ThielePattern) fillField(f *field.Field) {
+func (t *ThielePattern) remaind(c complex128) field.Point {
+	div := c / t.Base
+	intpart := cFloor(div)
+	floatpart := div - intpart
+	return cGauss(floatpart * t.Base)
+}
+
+func (t *ThielePattern) ApplyToField(f *field.Field) {
 	numbers := t.residueNumbers()
 
 	f.MapAsync(func(f *field.Field, x, y int, c field.Cell) field.Cell {
 		p := complex(float64(x), float64(y))
-		remaind := cRemaind(p, t.Base)
+		remaind := t.remaind(p)
 
 		for _, baseRemainder := range numbers {
 			if baseRemainder == remaind {
-				return field.LivingCell
+				return t.Cell
 				break
 			}
 		}
 
 		return c
 	})
-}
-
-func (t *ThielePattern) ApplyToField(f *field.Field) {
-
-	if t.Fill {
-		t.fillField(f)
-		return
-	}
-
-	for _, c := range t.residueNumbers() {
-		x := c.x + f.Width()/2
-		y := c.y + f.Height()/2
-		_ = f.SetAlive(x, y, t.Alive)
-	}
 }
