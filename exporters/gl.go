89d9578b490d8337bb40eab7916eec428e4978b3
Kim Ravn Hansen <krh@cego.dk>
2018-10-25 11:48:13 +0200

    opengl cradle

--- exporters/gl.go
+++ exporters/gl.go
@@ -3,10 +3,25 @@ package exporters
 import (
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
+	mgl "github.com/go-gl/mathgl/mgl32"
 	"github.com/moccalotto/nick/field"
 	"log"
-	"strings"
 )
+
+/**
+Note to self:
+OpenGL uses a right-handed.coordinate system.
+This means that:
+	when X increases, you move to the left,
+	when Y increases, you move up (which is inverse of how images are rendered, but matches how math graphs are normally displayed),
+	when Z increases, you move "towards the camera". This might be the opposite of what one would expect of a Z-axis.
+
+I want to map the "depth" of my field (i.e. the tallnes or value of the cell) to the Z-axis, so no issue there
+I want to map the width of my field to the X-axis, so no issue there either
+I want to map the height of my field to the Y-axis. I need to flip the Y coordinate so that Y_new := Height - Y_original
+
+Use this example: https://github.com/go-gl/example/blob/master/gl41core-cube/cube.go
+*/
 
 // ImageExporter exports images to files
 type GlExporter struct {
@@ -16,13 +31,13 @@ type GlExporter struct {
 	Title         string       // title of window
 	VertShaderSrc string       // source of vertex shader
 	FragShaderSrc string       // source of fragment shader
-	CellPoints    []float32    // The points (offsets rather) that make up a cell
 	prog          uint32       // handle to the opengl program
 	window        *glfw.Window // GLFW window.
 	vertArr       uint32       // handle to the vertex array object
 	vertCount     int32        // number of vertices
 	vertShader    uint32       // handle to compiled vertex shader
 	fragShader    uint32       // handle to compiled fragment shader
+	prevTickAt    float64      // time of last tick (glfw value)
 }
 
 func NewGlExporter(w, h int) *GlExporter {
@@ -34,29 +49,29 @@ func NewGlExporter(w, h int) *GlExporter {
 		// simplest vertex shader
 		VertShaderSrc: `
 			#version 410
-			in vec3 vp;
+
+			uniform mat4 projection;
+			uniform mat4 camera;
+			uniform mat4 model;
+			in vec3 vert;
+			in vec2 vertTexCoord;
+			out vec2 fragTexCoord;
 			void main() {
-				gl_Position = vec4(vp, 1.0);
-			}` + "\x00",
+				fragTexCoord = vertTexCoord;
+				gl_Position = projection * camera * model * vec4(vert, 1);
+			}
+			` + "\x00",
 
 		// simplest fragment shader. White color
 		FragShaderSrc: `
 			#version 410
-			out vec4 frag_colour;
+			uniform sampler2D tex;
+			in vec2 fragTexCoord;
+			out vec4 outputColor;
 			void main() {
-				frag_colour = vec4(1, 1, 1, 1.0);
-			}` + "\x00",
-		CellPoints: []float32{
-			// Bottom left right-angle triangle
-			-1, 1, 0,
-			1, -1, 0,
-			-1, -1, 0,
-
-			// Top right right-angle triangle
-			-1, 1, 0,
-			1, 1, 0,
-			1, -1, 0,
-		},
+			    outputColor = texture(tex, fragTexCoord);
+			}
+			` + "\x00",
 	}
 }
 
@@ -65,8 +80,10 @@ func (e *GlExporter) Export(f *field.Field) {
 
 	e.init(f)
 
+	e.prevTickAt = glfw.GetTime()
+
 	for !e.window.ShouldClose() {
-		e.update()
+		e.tick()
 	}
 }
 
@@ -77,75 +94,17 @@ func (e *GlExporter) cleanup() {
 }
 
 func (e *GlExporter) init(f *field.Field) {
-	e.initWindow()
-	e.initOpenGL()
-	points := e.makePoints(f)
-	e.initVertices(points)
+	e.initGlfw()   // create a window
+	e.initOpenGL() // start opengl
 }
 
-func (e *GlExporter) makePoints(f *field.Field) []float32 {
-	square := []float32{
-		// Bottom left right-angle triangle
-		-1, 1, 0,
-		1, -1, 0,
-		-1, -1, 0,
-
-		// Top right right-angle triangle
-		-1, 1, 0,
-		1, 1, 0,
-		1, -1, 0,
-	}
-
-	points := []float32{}
-	w := f.Width()
-	h := f.Height()
-
-	f.Walk(func(x, y int, c field.Cell) {
-		if c.Dead() {
-			return
-		}
-
-		for i := 0; i < len(square); i++ {
-			var factor float32
-			var size float32
-			switch i % 3 {
-			case 0:
-				// x-coordinates are at 0, 3, 6, etc.
-				// stretch cell to fit into viewport,
-				// which as width of 1.0
-				size = 1.0 / float32(w)
-				factor = float32(x) * size
-			case 1:
-				// y-coordinates are at 1, 4, 7, etc.
-				// stretch cell to fit into viewport,
-				// which as height of 1.0
-				size = 1.0 / float32(h)
-				factor = float32(y) * size
-			default:
-				// z-coordinates are at 2,5,8, etc.
-				// and we skip them.
-				points = append(points, 0.0)
-				continue
-			}
-
-			if square[i] < 0 {
-				points = append(points, factor*2-1)
-			} else {
-				points = append(points, ((factor+size)*2)-1)
-			}
-		}
-	})
-
-	return points
-}
-
-func (e *GlExporter) initWindow() {
+func (e *GlExporter) initGlfw() {
 	if err := glfw.Init(); err != nil {
 		log.Fatal(err)
 	}
 
 	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 4) // OR 2
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
 	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
@@ -165,60 +124,55 @@ func (e *GlExporter) initOpenGL() {
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	log.Println("OpenGL version", version)
 
-	e.vertShader = e.compileShader(&e.VertShaderSrc, gl.VERTEX_SHADER)
-	e.fragShader = e.compileShader(&e.FragShaderSrc, gl.FRAGMENT_SHADER)
-
 	e.prog = gl.CreateProgram()
-	gl.AttachShader(e.prog, e.vertShader)
-	gl.AttachShader(e.prog, e.fragShader)
+	e.vertShader = e.loadShader(&e.VertShaderSrc, gl.VERTEX_SHADER)
+	e.fragShader = e.loadShader(&e.FragShaderSrc, gl.FRAGMENT_SHADER)
 	gl.LinkProgram(e.prog)
 }
 
-func (e *GlExporter) update() {
+func (e *GlExporter) tick() {
+	// clear
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+	// updates
+	now := glfw.GetTime()
+	elapsed := now - e.prevTickAt
+	e.prevTickAt = now
+	angle += elapsed
+	/* model = */
+	mgl.HomogRotate3D(float32(angle), mgl.Vec3{0, 1, 0})
+
+	// render
 	gl.UseProgram(e.prog)
 
-	gl.BindVertexArray(e.vertArr)
-	gl.DrawArrays(gl.TRIANGLES, 0, e.vertCount)
-
-	glfw.PollEvents()
 	e.window.SwapBuffers()
+	glfw.PollEvents()
 }
 
-// makeVao initializes and returns a vertex array from the points provided.
-func (e *GlExporter) initVertices(points []float32) {
-	e.vertCount = int32(len(points)) / 3
-	var vbo uint32
-	gl.GenBuffers(1, &vbo)
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
-
-	gl.GenVertexArrays(1, &e.vertArr)
-	gl.BindVertexArray(e.vertArr)
-	gl.EnableVertexAttribArray(0)
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
-}
-
-func (e *GlExporter) compileShader(source *string, shaderType uint32) uint32 {
-	shaderHandle := gl.CreateShader(shaderType)
-
-	csources, freeFunc := gl.Strs(*source)
-	gl.ShaderSource(shaderHandle, 1, csources, nil)
-	freeFunc()
-	gl.CompileShader(shaderHandle)
-
-	var status int32
-	gl.GetShaderiv(shaderHandle, gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetShaderiv(shaderHandle, gl.INFO_LOG_LENGTH, &logLength)
-
-		msg := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(shaderHandle, logLength, nil, gl.Str(msg))
-
-		log.Fatalf("failed to compile %v: %v", *source, msg)
+func (e *GlExporter) loadShader(source *string, shaderType uint32) {
+	if e.prog == 0 {
+		log.Fatal("Program not initialized yet! Besure to call CreateProgram")
 	}
 
-	return shaderHandle
+	shaderHandle := gl.CreateShader(shaderType)
+
+	src, free := gl.Strs(*source)
+	defer free()
+
+	gl.ShaderSource(shaderHandle, 1, src, nil)
+	gl.CompileShader(shaderHandle)
+
+	var success int32
+	gl.GetShaderiv(shaderHandle, gl.COMPILE_STATUS, &success)
+
+	if success == gl.FALSE {
+		var logLen int32
+		gl.GetShaderiv(shaderHandle, gl.INFO_LOG_LENGTH, &logLen)
+
+		infoLog := make([]byte, logLen)
+		gl.GetShaderInfoLog(shaderHandle, logLen, nil, &infoLog[0])
+		log.Fatalf("error compiling vertex shader: %s", string(infoLog))
+	}
+
+	gl.AttachShader(e.prog, shaderHandle)
 }
