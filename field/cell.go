32c7e09c9b1f5ff9b6dcde66b30af07039925d85
Kim Ravn Hansen <krh@cego.dk>
2018-10-29 13:59:37 +0100

    Cells are bools - layers are out again

--- field/cell.go
+++ field/cell.go
@@ -1,26 +1,18 @@
 package field
 
-import (
-	"image/color"
-)
-
-type Cell color.RGBA
+type Cell bool
 
 type CellMapper func(f *Field, x, y int, c Cell) Cell
 
 type CellWalker func(x, y int, c Cell)
 
 func (c Cell) Alive() bool {
-	// Cells with value 1 or greater are considered to be alive
-	return c.A == 0xff
+	return c == LivingCell
 }
 
 func (c Cell) Dead() bool {
-	// Cells with value 0 or lower are considered to be dead.
-	// This means that there are several "dead" states.
-	// It also means that you cannot use fields to denote negative heights.
-	return c.A == 0x00
+	return c == DeadCell
 }
 
-var DeadCell Cell = Cell{R: 0x00, G: 0x00, B: 0x00, A: 0x00}
-var LivingCell Cell = Cell{R: 0xff, G: 0xff, B: 0xff, A: 0xff}
+const DeadCell Cell = false
+const LivingCell Cell = true
