1a739904f198a0002c6246d262885ea8a31a2246
Kim Ravn Hansen <krh@cego.dk>
2018-10-30 15:28:46 +0100

    New Scale instructions

--- machine/scale-instr.go
+++ machine/scale-instr.go
@@ -7,6 +7,8 @@ import (
 func init() {
 	InstructionHandlers["scale"] = Scale
 	InstructionHandlers["scale-to"] = ScaleTo
+	InstructionHandlers["scale-to-width"] = ScaleToWidth
+	InstructionHandlers["scale-to-height"] = ScaleToHeight
 }
 
 func Scale(m *Machine) {
@@ -27,6 +29,40 @@ func ScaleTo(m *Machine) {
 	newW := m.ArgAsInt(0)
 	m.Assert(m.ArgAsString(1) == "x", "Second arg to scale must be an 'x', but '%s' was given", m.ArgAsString(1))
 	newH := m.ArgAsInt(2)
+
+	effects.NewScaleTo(
+		m.Field.Width(),
+		newW,
+		m.Field.Height(),
+		newH,
+	).ApplyToField(m.Field)
+}
+
+func ScaleToWidth(m *Machine) {
+	m.Assert(m.Field != nil, "Cannot scale a non-initialized field!")
+
+	aspect := m.Field.AspectRatio()
+
+	newW := m.ArgAsInt(0)
+
+	newH := int(aspect / float64(newW))
+
+	effects.NewScaleTo(
+		m.Field.Width(),
+		newW,
+		m.Field.Height(),
+		newH,
+	).ApplyToField(m.Field)
+}
+
+func ScaleToHeight(m *Machine) {
+	m.Assert(m.Field != nil, "Cannot scale a non-initialized field!")
+
+	aspect := m.Field.AspectRatio()
+
+	newH := m.ArgAsInt(0)
+
+	newW := int(aspect * float64(newH))
 
 	effects.NewScaleTo(
 		m.Field.Width(),
