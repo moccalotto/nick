ff260ac974bb70e8317d7cf8fbba9a15ec4ae8c1
Kim Ravn Hansen <krh@cego.dk>
2018-10-30 16:39:24 +0100

    New loop-instructions.
    We can now break and continue.

--- machine/loop-instr.go
+++ machine/loop-instr.go
@@ -1,8 +1,16 @@
 package machine
 
+var skipUntilLoop map[string]bool = map[string]bool{
+	"endloop":    true,
+	"loop":       true,
+	"break-loop": true,
+}
+
 func init() {
 	InstructionHandlers["loop"] = Loop
 	InstructionHandlers["endloop"] = EndLoop
+	InstructionHandlers["continue"] = EndLoop
+	InstructionHandlers["break-loop"] = BreakLoop
 }
 
 func Loop(m *Machine) {
@@ -27,6 +35,37 @@ func EndLoop(m *Machine) {
 		return
 	}
 	next := m.State.PC
+
+	m.PopState()
+	m.State.PC = next
+}
+
+func Continue(m *Machine) {
+	m.State.Loop--
+	if m.State.Loop > 0 {
+		m.State.PC = m.State.Return
+		return
+	}
+
+	BreakLoop(m)
+}
+
+func BreakLoop(m *Machine) {
+	level := 1
+	next := 0
+	for i := m.State.PC; i < len(m.Tape); i++ {
+		if m.Tape[i].Cmd == "loop" {
+			level++
+		}
+
+		if m.Tape[i].Cmd == "endloop" {
+			level--
+		}
+
+		if level == 0 {
+			next = m.State.PC + i
+		}
+	}
 
 	m.PopState()
 	m.State.PC = next
