fa59528e3cc3b167b0cb0456a44fc47c6c394c1e
Kim Ravn Hansen <krh@cego.dk>
2018-10-31 15:53:45 +0100

    Speedup

--- effects/small-area-remover.go
+++ effects/small-area-remover.go
@@ -19,19 +19,19 @@ func (m *SmallAreaCRemover) ApplyToField(f *field.Field) {
 	}
 
 	w, h := f.Width(), f.Height()
+
 	// buffer to keep track of all the fields we've looked at
-	inspected := field.NewField(w, h)
+	inspected := make([]bool, w*h)
+	rawCells := f.Cells()
 
 	for y := 0; y < h; y = y + 1 {
 		for x := 0; x < w; x = x + 1 {
 			// ensure we don't look at the same cell twice
-			if a, err := inspected.On(x, y); err != nil {
-				panic(err)
-			} else if a {
+			if inspected[x+y*w] {
 				continue
 			}
 
-			curState, _ := f.On(x, y)
+			toggled := rawCells[x+y*w].Toggled()
 
 			// retrieve all cells in the given area.
 			a, err := f.GetAreaAround(x, y)
@@ -44,14 +44,14 @@ func (m *SmallAreaCRemover) ApplyToField(f *field.Field) {
 				// the area was too small. Remove it,
 				// and mark its cells as inspected
 				for _, p := range a {
-					_ = inspected.SetOn(p.X, p.Y, true)
-					_ = f.SetOn(p.X, p.Y, !curState)
+					inspected[p.X+p.Y*w] = true
+					rawCells[p.X+p.Y*w] = toggled
 				}
 			} else {
 				// the area was large enough to keep,
 				// but we still mark the area as inspected.
 				for _, p := range a {
-					_ = inspected.SetOn(p.X, p.Y, true)
+					inspected[p.X+p.Y*w] = true
 				}
 			}
 		}
