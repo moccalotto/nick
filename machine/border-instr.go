fbea4817e63bcd0e42ca474a2ca0e1f1bbe6666e
Kim Ravn Hansen <krh@cego.dk>
2018-11-02 15:50:52 +0100

    Border now allows relative border width

--- machine/border-instr.go
+++ machine/border-instr.go
@@ -13,8 +13,20 @@ func Border(m *Machine) {
 	m.Assert(m.Cave != nil, "The '%s' instruction can only be used after the 'init'", m.CurrentInstruction().Cmd)
 
 	border := effects.NewBorderSnow(1.0, m.Rng)
-	border.Thickness = m.ArgAsInt(0)
-	m.Assert(border.Thickness > 0, "Thickness must be > 0")
+
+	thickness := m.ArgAsFloat(0)
+	m.Assert(thickness > 0, "Thickness must be > 0")
+
+	// if the thickness is a float in the range (0, 1), it will be converted to
+	// a percentage of the smallest diameter of the cave. So if the thickness is
+	// .10, then the border size (in cells) will be 10% of the lower of the width or height,
+	if thickness < 1.0 && m.Cave.Width() > m.Cave.Height() {
+		border.Thickness = int(thickness * float64(m.Cave.Height()))
+	} else if thickness < 1.0 {
+		border.Thickness = int(thickness * float64(m.Cave.Width()))
+	} else {
+		border.Thickness = int(thickness)
+	}
 
 	if m.HasArg(1) {
 		border.Coverage = m.ArgAsFloat(1)
