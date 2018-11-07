184cf16e243e54906c0e316af77cee0eebd7b1fb
Kim Ravn Hansen <krh@cego.dk>
2018-11-07 16:17:08 +0100

    Support for 0xXXX and 0xXXXXXX colors

--- utils/color.go
+++ utils/color.go
@@ -12,6 +12,9 @@ func ParseColorString(s string) (color color.NRGBA, err error) {
 	if strings.HasPrefix(s, "#") {
 		return ParseHexColorString(s)
 	}
+	if strings.HasPrefix(s, "0x") {
+		return ParseHexColorString(s)
+	}
 	if strings.HasPrefix(s, "rgba(") {
 		return ParseRgbaString(s)
 	}
@@ -28,23 +31,28 @@ func ParseHexColorString(s string) (col color.NRGBA, err error) {
 	s = strings.ToLower(s)
 	col.A = 255
 
-	if len(s) == 7 {
+	switch len(s) {
+	case 8:
+		_, err = fmt.Sscanf(s, "0x%02x%02x%02x", &col.R, &col.G, &col.B)
+	case 7:
 		_, err = fmt.Sscanf(s, "#%02x%02x%02x", &col.R, &col.G, &col.B)
-		return
-	}
-
-	if len(s) == 4 {
+	case 5:
+		_, err = fmt.Sscanf(s, "0x%1x%1x%1x", &col.R, &col.G, &col.B)
+		if err == nil {
+			col.R *= 17
+			col.G *= 17
+			col.B *= 17
+		}
+	case 4:
 		_, err = fmt.Sscanf(s, "#%1x%1x%1x", &col.R, &col.G, &col.B)
 		if err == nil {
 			col.R *= 17
 			col.G *= 17
 			col.B *= 17
 		}
-		return
+	default:
+		err = fmt.Errorf("Invalid hex color: '%s'", s)
 	}
-
-	err = fmt.Errorf("Invalid hex color: '%s'", s)
-
 	return
 }
 
--- utils/color_test.go
+++ utils/color_test.go
@@ -8,9 +8,12 @@ import (
 func TestColorParsers(t *testing.T) {
 
 	tests := map[string]color.NRGBA{
-		"rgba(255, 255, 255, 0xff)": color.NRGBA{255, 255, 255, 255},
-		"rgb(123, 0xff, 00)":        color.NRGBA{123, 255, 0, 255},
-		"#ff00ff":                   color.NRGBA{255, 00, 255, 255},
+		"rgba(255, 255, 255, 0xff)": color.NRGBA{0xff, 0xff, 0xff, 0xff},
+		"rgb(123, 0xff, 00)":        color.NRGBA{0x7b, 0xff, 0x00, 0xff},
+		"#ff00ff":                   color.NRGBA{0xff, 0x00, 0xff, 0xff},
+		"#fa0":                      color.NRGBA{0xff, 0xaa, 0x00, 0xff},
+		"0xff00ff":                  color.NRGBA{0xff, 0x00, 0xff, 0xff},
+		"0xfa0":                     color.NRGBA{0xff, 0xaa, 0x00, 0xff},
 	}
 
 	for s, res := range tests {
