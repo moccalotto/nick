a308c10d092f12a3fc1d8e50782afe3956917b0b
Kim Ravn Hansen <krh@cego.dk>
2018-11-08 14:39:15 +0100

    Add FindClosestArea

--- field/area.go
+++ field/area.go
@@ -24,3 +24,27 @@ func (this Area) FindClosestPoints(other Area) (Point, Point, uint64) {
 
 	return bestP1, bestP2, bestDistSq
 }
+
+func (this Area) FindClosestArea(areas []Area) (Area, Point, Point, uint64) {
+	bestDistSq := uint64(0xFFFFFFFFFFFF)
+	var bestArea Area
+	var bestP1 Point
+	var bestP2 Point
+
+	for _, other := range areas {
+		p1, p2, distSq := this.FindClosestPoints(other)
+
+		if distSq == 0 {
+			return other, p1, p2, distSq
+		}
+
+		if distSq < bestDistSq {
+			bestDistSq = distSq
+			bestP1 = p1
+			bestP2 = p2
+			bestArea = other
+		}
+	}
+
+	return bestArea, bestP1, bestP2, bestDistSq
+}
