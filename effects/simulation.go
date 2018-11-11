ec250e275d87baba3f825d70597a748a8166baa5
Kim Ravn Hansen <krh@cego.dk>
2018-11-11 14:20:39 +0100

    simulation:map support

--- effects/simulation.go
+++ effects/simulation.go
@@ -9,14 +9,20 @@ type Transformation struct {
 	SourceFilter    map[field.Cell]bool // The transformation only runs if the source cell is represented in this map.
 	Coverage        float64             // The transformation only runs if a die roll [0, 1) is lower than this number.
 	TargetCell      field.Cell          // The transformation returns this cell if all matches occur.
-	CheckNeighbours bool                // If true, this filter must also match the number of neighbours of a particular type.
-	NeighbourType   field.Cell          // The transformation only runs if the number of neighbours of the given type
+	CheckNeighbours bool                // If true, this filter must also match the number of neighbours of the given types.
+	NeighbourTypes  []field.Cell        // The transformation only runs if the number of neighbours of the given type
 	NeighbourCounts [9]bool             //   is represented in NeighbourCounts.
 }
 
 type Simulation struct {
 	rng             *rand.Rand
 	Transformations []Transformation
+}
+
+func NewSimulation(rng *rand.Rand) *Simulation {
+	return &Simulation{
+		rng: rng,
+	}
 }
 
 func (this *Simulation) ApplyToField(f *field.Field) {
@@ -38,19 +44,26 @@ func (this *Simulation) ApplyToField(f *field.Field) {
 			}
 
 			// The transformation does not care about the neighbours, so it passes all filters.
-			if !trans.CheckNeighbours {
+			if len(trans.NeighbourTypes) == 0 {
 				// return now, because only one transformation can apply to a cell
 				return trans.TargetCell
 			}
 
 			// The transformation wants to know about our neighbours, so we load that info into the cache.
-			if trans.CheckNeighbours && len(countCache) == 0 {
+			if len(countCache) == 0 {
 				countCache = f.NeighbourCountByType(x, y)
 			}
 
-			neighbourCount := countCache[trans.NeighbourType]
+			// neighbourCount is the number of neighbours that is of one of the types
+			// given in NeighbourTypes
+			neighbourCount := 0
 
-			// The transformation matches the number of neighbours of the given type
+			// add up the neighbours of the matching types.
+			for _, cellType := range trans.NeighbourTypes {
+				neighbourCount += countCache[cellType]
+			}
+
+			// The transformation matches the number of neighbours of the given types
 			if trans.NeighbourCounts[neighbourCount] {
 				return trans.TargetCell
 			}
--- machine/simulation-instr.go
+++ machine/simulation-instr.go
@@ -3,35 +3,127 @@ package machine
 import (
 	"github.com/moccalotto/nick/effects"
 	"github.com/moccalotto/nick/field"
+	"strconv"
+	"strings"
 )
 
 func init() {
-	InstructionHandlers["simulation"] = Simulation
+	InstructionHandlers["simulation"] = StartSimulation
 	InstructionHandlers["endsimulation"] = EndSimulation
 	InstructionHandlers["commit"] = Commit
+	InstructionHandlers["apply"] = ApplySimulation
+	InstructionHandlers["map"] = Map
 }
 
-var simQueues map[*Machine][]Simulation
+var simStacks map[*Machine][]*effects.Simulation = map[*Machine][]*effects.Simulation{}
 
-func Simulation(m *Machine) {
-	// SourceFilter    map[field.Cell]bool // The transformation only runs if the source cell is represented in this map.
-	// Coverage        float64             // The transformation only runs if a die roll [0, 1) is lower than this number.
-	// TargetCell      field.Cell          // The transformation returns this cell if all matches occur.
-	// CheckNeighbours bool                // If true, this filter must also match the number of neighbours of a particular type.
-	// NeighbourType   field.Cell          // The transformation only runs if the number of neighbours of the given type
-	// NeighbourCounts [9]bool             //   is represented in NeighbourCounts.
+func currentSim(m *Machine) *effects.Simulation {
+	// pop the simulation off of the stack and execute it
+	stack, ok := simStacks[m]
 
-	newSimulation := effects.Simulation{
-		SourceFilter: map[field.Cell]bool{},
-		Coverage:     1.0,
-	}
-	simQueues[m] = append(newSimulation, simQueues)
+	m.Assert(ok, "No simulation started")
+	m.Assert(len(stack) > 0, "Not in a simulation")
+
+	topOpStack := len(stack) - 1
+
+	return stack[topOpStack]
+}
+
+func StartSimulation(m *Machine) {
+	newSimulation := effects.NewSimulation(m.Rng)
+	simStacks[m] = append(simStacks[m], newSimulation)
 }
 
 func EndSimulation(m *Machine) {
-	// pop the simulation off of the queue and execute it
+	ApplySimulation(m)
+	stack := simStacks[m]
+	simStacks[m] = stack[0 : len(stack)-1]
 }
 
 func Commit(m *Machine) {
 	// end + start new
+	EndSimulation(m)
+	StartSimulation(m)
+}
+
+func ApplySimulation(m *Machine) {
+	currentSim(m).ApplyToField(m.Cave)
+}
+
+// map Alive nextTo 4,5,6,7,8 * Alive => Dead    # Death from overpopulation
+func Map(m *Machine) {
+
+	// arg: type
+	// 0: SourceFilter
+	// 1: "nextTo"
+	// 2: NeighbourCounts
+	// 3: "*"
+	// 4: NeighbourTypes
+	// 5: "=>"
+	// 6: TargetCell
+
+	m.Assert(m.ArgAsString(1) == "nextTo", "second arg must be 'nextTo'")
+	m.Assert(m.ArgAsString(3) == "*", "fourth arg must be '*'")
+	m.Assert(m.ArgAsString(5) == "=>", "sixth arg must be '=>'")
+
+	t := effects.Transformation{}
+
+	if len(simStacks[m]) == 0 {
+		m.Assert(len(simStacks[m]) > 0, "Simulation not started")
+	}
+
+	topOfStack := len(simStacks[m]) - 1
+
+	simStacks[m][topOfStack].Transformations = append(
+		simStacks[m][topOfStack].Transformations,
+		t,
+	)
+
+	sourcesStr := m.ArgAsString(0)
+	t.SourceFilter = map[field.Cell]bool{}
+	t.Coverage = 1.0
+	for _, cellAlias := range strings.Split(sourcesStr, ",") {
+		if cell, ok := m.CellNames[cellAlias]; ok {
+			t.SourceFilter[cell] = true
+		} else if num, err := strconv.Atoi(cellAlias); err == nil {
+			t.SourceFilter[field.Cell(num)] = true
+		} else {
+			panic(err)
+		}
+	}
+
+	nCountStr := m.ArgAsString(2)
+
+	for _, numStr := range strings.Split(nCountStr, ",") {
+		if i, err := strconv.Atoi(numStr); err == nil {
+			t.NeighbourCounts[i] = true
+		} else {
+			panic(err)
+		}
+	}
+
+	neighboursStr := m.ArgAsString(4)
+	t.NeighbourTypes = []field.Cell{}
+	t.Coverage = 1.0
+	for _, cellAlias := range strings.Split(neighboursStr, ",") {
+		if cell, ok := m.CellNames[cellAlias]; ok {
+			t.NeighbourTypes = append(t.NeighbourTypes, cell)
+		} else if num, err := strconv.Atoi(cellAlias); err == nil {
+			t.NeighbourTypes = append(t.NeighbourTypes, field.Cell(num))
+		} else {
+			panic(err)
+		}
+	}
+
+	targetCellStr := m.ArgAsString(6)
+
+	if cell, ok := m.CellNames[targetCellStr]; ok {
+		t.TargetCell = cell
+	} else {
+		if num, err := strconv.Atoi(targetCellStr); err == nil {
+			t.TargetCell = field.Cell(num)
+		} else {
+			panic(err)
+		}
+	}
 }
